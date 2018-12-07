ed751aa11c9269cf514183addcff81e26e9ae5fc
Joe Chellman <joe@shooflydesign.org>
2018-12-06 17:01:29 -0800

    More ch5 stuff

--- Ch05/05_01/transcript.js
+++ Ch05/05_01/transcript.js
@@ -1,27 +1,41 @@
 for (var i = 0; i < 10; i += 1) {
-	console.log(i);
+  console.log(i);
 }
 
 // very common use case: looping over an array.
-var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog'];
+var pageNames = [
+  "Home",
+  "About Us",
+  "Contact Us",
+  "JavaScript Playground",
+  "News",
+  "Blog"
+];
 for (i = 0; i < pageNames.length; i += 1) {
-	if (document.title == pageNames[i]) {
-		console.log('We ARE here: ' + pageNames[i]);
-	} else {
-		console.log('We are not here: ' + pageNames[i]);
-	}
+  if (document.title === pageNames[i]) {
+    console.log("We ARE here: " + pageNames[i]);
+  } else {
+    console.log("We are not here: " + pageNames[i]);
+  }
 }
 
 // don't repeat yourself:
-var pageNames = ['Home', 'About Us', 'Contact Us', 'JavaScript Playground', 'News', 'Blog'];
+var pageNames = [
+  "Home",
+  "About Us",
+  "Contact Us",
+  "JavaScript Playground",
+  "News",
+  "Blog"
+];
 for (i = 0; i < pageNames.length; i += 1) {
-	var currentPageTitle = pageNames[i];
+  var currentPageTitle = pageNames[i];
 
-	if (document.title == currentPageTitle) {
-		console.log('We ARE here: ' + currentPageTitle);
-	} else {
-		console.log('We are not here: ' + currentPageTitle);
-	}
+  if (document.title === currentPageTitle) {
+    console.log("We ARE here: " + currentPageTitle);
+  } else {
+    console.log("We are not here: " + currentPageTitle);
+  }
 }
 
 // More info:
